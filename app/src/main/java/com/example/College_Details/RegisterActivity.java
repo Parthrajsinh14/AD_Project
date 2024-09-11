b13c838bafb07fe824198f2861a0a807f6602fb5
Harshil <harshil.karia.hk@gmail.com>
2024-09-12 00:40:32 +0530

    Completed Static Login

--- app/src/main/java/com/example/College_Details/RegisterActivity.java
+++ app/src/main/java/com/example/College_Details/RegisterActivity.java
@@ -1,10 +1,12 @@
 package com.example.College_Details;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,15 +17,40 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText Username;
     private EditText Password;
     Button Reg;
+    private TextView loginPage;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+        sharedPreferences = getSharedPreferences("login_details",MODE_PRIVATE);
+        boolean loggedIn = sharedPreferences.getBoolean("isLoggedIn",false);
+
+        if(loggedIn){
+            intent = new Intent(RegisterActivity.this,DashBoard.class);
+            startActivity(intent);
+            finish();
+        }
+
         Username = findViewById(R.id.username1);
         Password = findViewById(R.id.password1);
+        loginPage = findViewById(R.id.loginPage);
         Reg = findViewById(R.id.register);
+        editor = sharedPreferences.edit();
+
+
+        loginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         Reg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +61,30 @@ public class RegisterActivity extends AppCompatActivity {
                 if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(RegisterActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
                 } else {
+
+                    //Using temporary Data
+
+                    if (username.equals("admin") && password.equals("admin")) {
+                        // Proceed to the next activity or dashboard
+                        editor.putString("username",username);
+                        editor.putBoolean("isLoggedIn",true);
+                        editor.commit();
+                        Toast.makeText(RegisterActivity.this, "Registered Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegisterActivity.this, DashBoard.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Invalid Details", Toast.LENGTH_SHORT).show();
+                    }
+
+
+
                     // Handle the signup logic here
-                    DBHandler dbHandler = new DBHandler(RegisterActivity.this);
-                    dbHandler.insertUserDetails(username, password);
-                    Toast.makeText(RegisterActivity.this, "Registered successfully!", Toast.LENGTH_SHORT).show();
-                    intent = new Intent(RegisterActivity.this, MainActivity.class);
-                    startActivity(intent);
+//                    DBHandler dbHandler = new DBHandler(RegisterActivity.this);
+//                    dbHandler.insertUserDetails(username, password);
+//                    Toast.makeText(RegisterActivity.this, "Registered successfully!", Toast.LENGTH_SHORT).show();
+//                    intent = new Intent(RegisterActivity.this, MainActivity.class);
+//                    startActivity(intent);
                     // You can also start another activity here if needed
                 }
             }
