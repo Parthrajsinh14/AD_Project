d3ccd963a5aa75fc7caa26c4984364c227b5adab
Harshil <harshil.karia.hk@gmail.com>
2024-09-15 14:54:18 +0530

    Completed Read Opreation for college list

--- app/src/main/java/com/example/College_Details/CollegeList.java
+++ app/src/main/java/com/example/College_Details/CollegeList.java
@@ -7,9 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CollegeList extends AppCompatActivity {
-
+    CollegeAdapter collegeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,29 @@ public class CollegeList extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewColleges);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<CollegeModel> options =
+                new FirebaseRecyclerOptions.Builder<CollegeModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("college"),CollegeModel.class)
+                        .build();
+
+        collegeAdapter = new CollegeAdapter(options);
+        recyclerView.setAdapter(collegeAdapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        collegeAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        collegeAdapter.startListening();
     }
 }
